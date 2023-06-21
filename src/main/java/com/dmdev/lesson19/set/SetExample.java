fa49c3d7eb6c5cbb450570d63480ae17d1a2dea4
Viner108 <andrukovichl@mail.ru>
2023-06-21 12:55:47 +0300

    video 15(коллекция) commit

---
+++ src/main/java/com/dmdev/lesson19/set/SetExample.java
@@ -0,0 +1,20 @@
+package com.dmdev.lesson19.set;
+
+import java.util.*;
+
+public class SetExample {
+    public static void main(String[] args) {
+        new HashSet<>();
+        new LinkedHashSet<>();
+        Set<String> set=new TreeSet<>();
+        System.out.println(set.add("aaa"));
+        System.out.println(set.add("bbb"));
+        System.out.println(set.add("ccc"));
+        System.out.println(set.add("ddd"));
+        System.out.println(set.add("aaa"));
+        System.out.println(set);
+        for (String value : set) {
+            System.out.println(value);
+        }
+    }
+}
